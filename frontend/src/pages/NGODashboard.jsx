ef25f7ac9122858d4265de8d98187025ac3ef81e
Kanishkj92 <kanishkj92@gmail.com>
2025-04-03 23:31:53 +0530

    mp

--- frontend/src/pages/NGODashboard.jsx
+++ frontend/src/pages/NGODashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { logout } from "../redux/user/userSlice";
+
 import { useNavigate } from "react-router-dom";
 
 const NgoDashboard = () => {
